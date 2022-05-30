55a49fd3aa75efb804ab846c2a468d5f9f001caa
nni-odoo <nni@odoo.com>
2022-05-30 18:05:00 +0800

    Added compute and onchange mehtod

--- library/models/models.py
+++ library/models/models.py
@@ -1,4 +1,5 @@
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class Book(models.Model):
@@ -7,6 +8,14 @@ class Book(models.Model):
     name = fields.Char("Title")
     isbn = fields.Char("ISBN")
     copies = fields.Integer("Number of Copies")
+    remaining = fields.Integer("Remaining Copies", compute="_compute_remaining", store=True)
+
+    @api.depends('copies')
+    def _compute_remaining(self):
+        # copies - rentals that refer to this book
+        for record in self:
+            total_rentals = self.env['library.rental'].search_count([('book', '=', record.id)])
+            record.remaining = record.copies - total_rentals
 
 
 class Customer(models.Model):
@@ -22,3 +31,10 @@ class Rental(models.Model):
     book = fields.Many2one("library.book", "Book")
     customer = fields.Many2one("library.customer", "Customer")
     rent_date = fields.Date("Rental Date")
+    end_date = fields.Date("Return Date")
+
+    @api.onchange('end_date')
+    def _onchange_end_date(self):
+        # can't allow end date set before rent_date
+        if self.rent_date and self.end_date < self.rent_date:
+            raise UserError("WARNING: You shouldn't set the Return Date to be Earlier than Rent Date")
